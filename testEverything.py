98610f779e3cc1cf618e385b02016afee6d545c7
simon <1115443660@qq.com>
2018-06-27 08:43:54 -0400

    try

---
+++ testEverything.py
@@ -0,0 +1,7 @@
+testVar = int(input("please rate on this course!!:\nFrom 1 to 10\nEnter below:\n"))
+if testVar>=1 and testVar<=3:
+    print("Your terrible reply has been sent to Cornell University officials")
+elif testVar>=4 and testVar<=6:
+    print("Your modest reply has been sent to Cornell University officials")
+else:
+    print("damn you!")
