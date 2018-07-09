aaffbc0436c3d7cc5e0f06600d1a398c323bca2f
Simon <1115443660@qq.com>
2018-07-09 14:38:08 -0400

    ，tdmytdmhgf

---
+++ HW3_Q1/Matrix.py
@@ -0,0 +1,5 @@
+class Matrix:
+    def __init__(self, rows, cols):
+        ###takes in the number of rows and cols needed for the matrix###
+        self.number_rows = rows
+        self.number_cols = cols
