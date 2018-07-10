a305f955f61cf65bcd1e886091593061a4b90dd7
simon <1115443660@qq.com>
2018-07-09 20:43:43 -0400

    ha

--- HW3_Q1/Matrix.py
+++ HW3_Q1/Matrix.py
@@ -3,3 +3,10 @@ class Matrix:
         ###takes in the number of rows and cols needed for the matrix###
         self.number_rows = rows
         self.number_cols = cols
+        self.rows_and_cols = [[0]*cols for i in range(rows)]
+
+    def get_matrix(self, matrix):
+        print(self.rows_and_cols)
+
+    def get_item(self, index_row, index_col):
+        print(self.rows_and_cols[index_row][index_col])
--- inClassContent/notes.py
+++ inClassContent/notes.py
@@ -1,26 +1,25 @@
-'''class Person:
-    '''This class will allow me to make people!
-    object attributes: my_name, my_gender, my_age, my_friends'''
+class Person:
+    '''This class will allow me to make people! object attributes: my_name, my_gender, my_age, my_friends'''
     def __init__(self,name,gender,age):
         self.my_name = name
         self.my_gender = gender
         self.my_age = age
         self.my_friends = []
-    def person_kind(): # WHY NO self?
+    def person_kind(self): # WHY NO self?
         if self.my_gender[0] == 'm':return 'lad'
         elif self.my_gender[0] == 'f':return 'lass'
         else: return 'martian'
     def introduce_myself(self): # WHY HAVE self?
         print("Hi, I'm " + self.my_name+", and I'm a "+ str(self.my_age)+"-year-old "+self.person_kind())
-    def add_friend(nice_person):
+    def add_friend(self,nice_person):
         self.my_friends.append(nice_person)
-    def show_friends():
+    def show_friends(self):
         return self.my_friends
         
 Simon = Person("Simon", "male", 17)
 Betty = Person("Betty", "female", 17)
 print(Simon.my_age)
-print(Betty.introduce_myself())'''
+print(Betty.introduce_myself())
 
 class Chair:
     def __init__(self, person = None, next = None):
